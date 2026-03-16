1cc3214cdef21fafd1acdbcfb527f3e48b448cae
Clav3rbot <domenico.pittari@mail.polimi.it>
2026-03-16 21:32:16 +0100

    fix: use server-side proxy for API calls

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-};
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ]
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
