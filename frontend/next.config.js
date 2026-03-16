da6f6f299217c2c11541b5334055fd292d27bfe9
Clav3rbot <domenico.pittari@mail.polimi.it>
2026-03-16 21:34:08 +0100

    Stability improvements and minor adjustments

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/:path*`,
+        destination: `${process.env.API_URL || 'http://127.0.0.1:8000'}/:path*`,
       },
     ]
   },
